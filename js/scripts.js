574f4064400d7b21e8544d948bcf37ccc7642b2a
Tristan Emmerson <tristan@stickerslug.com>
2020-06-04 13:43:29 -0700

    Add page header and form boxes for 3 sides of user input for triangle test.
    co-authored by: Micheal Hansen <micheal.hansen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
- 
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    const side1 = parseInt($("input#side1").val());
+    const side2 = parseInt($("input#side2").val());
+    const side3 = parseInt($("input#side3").val());
+
+    event.preventDefault();
+
+  });
+});
